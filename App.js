6b9188b3bfb96787be92b1f75324c4c34983d3b2
bharathmj32 <80873958+bharathmj32@users.noreply.github.com>
2022-01-09 12:12:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import Buyer_details from "./Pages/Buyer_details/buyer_details";
+// import Product from "./Pages/Product/product";
+
+function App() {
+  return (
+      <Buyer_details />
+  );
+}
+
+export default App;
---
+++ buyer_details.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import Navbar from '../../Components/FrequentComponents/navbar'
+import Footer from '../../Components/FrequentComponents/footer'
+import { Button, Grid,  Typography } from '@mui/material'
+import {  LockClock} from '@mui/icons-material'
+import "../Buyer_details/buyer_details.scss";
+import PersonIcon from '@mui/icons-material/Person';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
+
+export default function Buyer_details() {
+    return (
+        <>
+            <Grid container direction="column" className='buyer_details_container' sx={{width:'80%'}}>
+                {/* <Grid item> <Navbar /> </Grid> */}
+
+                <Grid item> <Typography gutterBottom variant='h4' className='buyer_details_title'>Address</Typography> </Grid>
+
+                <Grid item container direction="column" spacing={2} className='buyer_details_form'>  
+                    <Grid item container direction='row' spacing={5}> 
+                        <Grid item> 
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="First Name" />
+                                <PersonIcon className='i'/>
+                            </div>
+                        </Grid>
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="Last Name" />
+                                <PersonIcon className='i'/>
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item >
+                        <Button className='pick_loc_button' variant="outlined">Pick Location</Button>
+                    </Grid>
+                    <Grid item container direction='row' spacing={5}> 
+                        <Grid item> 
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="Address" />
+                                <LocationOnIcon className='i'/>
+                            </div>
+                        </Grid>
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="City" />
+                                <LocationOnIcon className='i'/>
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item container direction='row' spacing={5}> 
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="State" />
+                                <LocationOnIcon className='i'/>
+                            </div>
+                        </Grid>
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="text" placeholder="Pincode" />
+
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item container direction='row' spacing={5}> 
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="tel" placeholder="Enter Your Number" />
+                                <CallIcon className='i'/>
+                            </div>
+                        </Grid>
+                        <Grid item>
+                            <div class="inputWithIcon">
+                                <input type="email" placeholder="Email" />
+                                <EmailIcon className='i'/>
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item >
+                        <Button variant="contained" startIcon={<LockClock />} className='add_cart'>
+                        Back to Cart
+                        </Button>
+                    </Grid>
+
+                </Grid>
+                    
+
+                {/* <Grid item> <Footer /> </Grid> */}
+            </Grid>
+        </>
+    )
+}
+
+
+                    
